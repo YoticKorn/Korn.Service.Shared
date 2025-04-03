e22a7b95e520da7a9cb79f45bced8fc895854c97
Yotic <yoticcitoy@gmail.com>
2025-04-03 21:48:14 +0300

    Passes the tests, implemented whole functional

--- Korn.Service.Shared/Packet.cs
+++ Korn.Service.Shared/Packet.cs
@@ -2,15 +2,43 @@
 {
     public abstract class Packet { }
 
-    public class ClientPacket : Packet { } // packet from client    
+    public class ClientPacket : Packet { } // packet from client
 
-    public class ClientPacketWithCallback<T> : ClientPacket where T : ServerCallbackPacket { }
-    
-    public class ClientCallbackPacket : ServerPacket { }
+    public class ClientCallbackablePacket : ClientPacket
+    {
+        public int CallbackID;
+    }
+
+    public class ClientCallbackablePacket<T> : ClientCallbackablePacket where T : ServerCallbackPacket { }
+
+    public class ClientCallbackPacket : ClientPacket
+    {
+        public int CallbackID;
+
+        public ClientCallbackPacket ApplyCallback(ServerCallbackablePacket packet)
+        {
+            CallbackID = packet.CallbackID;
+            return this;
+        }
+    }
 
     public class ServerPacket : Packet { } // packet from server
 
-    public class ServerPacketWithCallback<T> : ServerPacket where T : ClientCallbackPacket { }
+    public abstract class ServerCallbackablePacket : ServerPacket
+    {
+        public int CallbackID;
+    }
 
-    public class ServerCallbackPacket : ClientPacket { }
+    public class ServerCallbackablePacket<T> : ServerCallbackablePacket where T : ClientCallbackPacket { }
+
+    public class ServerCallbackPacket : ServerPacket
+    {
+        public int CallbackID;
+
+        public ServerCallbackPacket ApplyCallback(ClientCallbackablePacket packet)
+        {
+            CallbackID = packet.CallbackID;
+            return this;
+        }
+    }
 }
